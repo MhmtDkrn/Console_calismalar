5e5976d52b491d4b9dd4f2414a74f6d32dff0cb3
MhmtDkrn <mehmetdikran47@gmail.com>
2023-09-27 17:52:22 +0300

    deneme

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+byte b=5;
+sbyte c=5;
+
+short s=5;
+ushort us=5;
+
+Int16 i16=2; 
+int i=2;
+Int32 i32=2;
+Int64 i64=2;
+
+//ondalık
+double db=2.5;
+float fl=32;
+decimal dc=54;
+
+
+DateTime dt= DateTime.Now;
+Console.WriteLine(dt);
+string str= string.Empty;
+Console.WriteLine(str);
